69ef6a5b6982c1f4b6aa0b2dfdade7bf4c893abc
olra0312 <43948728+olra0312@users.noreply.github.com>
2020-05-05 11:38:52 +0200

    Project has been set up

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+const server = app.listen(port, (error) => {
+    if (error) {
+        console.log("Error starting the server");
+    }
+    console.log("This server is running on port", server.address().port);
+});
