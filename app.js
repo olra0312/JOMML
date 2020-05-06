814937104f8e6c10afda4ebb812325e7e29cb5a2
Mfajs2609 <Mfajs@hotmail.dk>
2020-05-06 11:44:06 +0200

    Created createuser.html

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 
 
+app.get("/createuser", (req, res) => {
+
+   return res.sendFile(__dirname + "/public/createuser/createuser.html");
+})
 
 
 
 
-const PORT = process.argv[2];
+
+const PORT = 3000;
 
 const server = app.listen(PORT, (error) => {
     if (error) {
