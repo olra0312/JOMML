3308cbaa7d041f99f2bd9af2c4d309907b684d44
maestro3m <miclomacario990@gmail.com>
2020-05-06 11:53:36 +0200

    Changes in knex and package.json

--- migrations/20200506110914_inital_schema.js
+++ migrations/20200506110914_inital_schema.js
@@ -2,10 +2,18 @@
 exports.up = function(knex) {
   return knex.schema
   .createTable("users", table => {
-      table.increments("id")
-      table.string("username").unique().notNullable()
-      table.string("password").notNullable()
+    table.string("first_name")
+    table.string("last_name")
+    table.increments("id")
+    table.string("username").unique().notNullable()
+    table.string("password").notNullable()
+    table.string("email")
+    table.bigInteger("phone_number")
+    table.string("address")
+    table.integer("zip_code")
+    table.string("city")
   })
+
 };
 
 exports.down = function(knex) {
