1b7d849b48103fb13071e1c1c721d3f364f00f4f
olra0312 <43948728+olra0312@users.noreply.github.com>
2020-05-07 11:46:42 +0200

    Finished login functionality and created a test home page

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 
+//GET requests
 app.get("/createuser", (req, res) => {
-
    return res.sendFile(__dirname + "/public/createuser/createuser.html");
 })
 
 app.get("/login", (req, res) => {
-
     return res.sendFile(__dirname + "/public/login/login.html");
  })
 
+ app.get("/home", (req, res) => {
+    return res.sendFile(__dirname + "/public/home/home.html");
+});
+
+//POST requests
 
 
 
+//LOGIN
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'password',
+	database : 'jomml_db'
+});
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+//Checks if the user input is the same as in the database//
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Forkert brugernavn eller password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Venligst indtast brugernavn og kodeord.');
+		response.end();
+	}
+});
+
+
+//////////////////////
 const PORT = process.argv[2];
 
 const server = app.listen(PORT, (error) => {
