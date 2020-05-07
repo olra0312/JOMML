d5651c764ee8443c9d2eec86dbd08d1f46ba577c
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-05-07 13:19:23 +0200

    Form data in create user binded to database.

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
+//Setting up the express library from NPM to create a server.
 const express = require("express");
 const app = express();
 
+//Passing jSON-objects and form data in HTML-files.
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+//User router reference.
+const userRouter = require('./routes/userRouter.js');
+app.use(userRouter);
+
+//Getting access to static files such as CSS, images, videos etc.
 app.use(express.static(__dirname + '/public'))
 
-
+//Defining objection model and knex library.
 const { Model } = require("objection")
 const Knex = require("knex")
 const knexfile = require("./knexfile.js")
 
+//Creating connection to database.
 const knex = Knex(knexfile.development)
-
 Model.knex(knex)
 
+//Defining file system as a standard library.
 const fs = require("fs")
 
 
-
-app.get("/createUser", (req, res) => {
-    const body = fs.readFileSync("./public/createUser/createUser.html", "utf8")
-
-   return res.send(body)
-})
-
-const User = require("./models/User.js");
-
-app.post("/createUser",(req, res) => {
-    try {
-    User.query().insert({ 
-        first_name: req.body.firstName, 
-        last_name: req.body.lastName,
-        email: req.body.email,
-        phone_number: req.body.phoneNumber,
-        address: req.body.address,
-        zip_code: req.body.zipCode,
-        city: req.body.city,
-        username: req.body.username,
-        password: req.body.password
-    }).then(createdUser => { 
-        return res.send( { response: `The user ${createdUser.username} was created`})
-    }); 
-
-    } catch {
-        
-    }
-
-    console.log(req.body)
-
-})
 
 
 
