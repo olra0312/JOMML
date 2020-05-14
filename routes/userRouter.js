369f493e29803cae73b03b58a6a157cc5ff018e0
Mfajs2609 <Mfajs@hotmail.dk>
2020-05-14 13:37:57 +0200

    Ready to test

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -30,22 +30,21 @@ router.post("/createUser",(req, res) => {
     try{
         //Password validation
         if (password && confirmPassword && firstName && lastName && email && phoneNumber && address && zipCode && city && username) {
-                
+            
+            const CapLetterRegex = /[A-Z]+/g
+            const NumbersRegex = /[0-9]/g
+            const EmailRegex = /[@]+/g
+            const PhoneRegex = /\d{8}/g
+            const ZipRegex = /\d{4}/g
+            const wordRegex = /\w+/g
+
             if (password && confirmPassword) {
-                    
-                const CapLetterRegex = /[A-Z]+/g
-                const NumbersRegex = /[0-9]/g
-                const EmailRegex = /[@]+/g
-                const PhoneRegex = /\d{8}/g
-                const ZipRegex = /\d{4}/g
 
                 if (password.length < 8) {
-                    //console.log("Password must be 8 characters or longer");
                     return res.status(400).send({ response: "Password must be 8 characters or longer"});
                 } 
                     
                 if (password !== confirmPassword) {
-                    //console.log("Password and confirm password needs to be the same");
                     return res.status(400).send({ response: "Password and confirm password needs to be the same"});
                 } 
                     
@@ -59,26 +58,22 @@ router.post("/createUser",(req, res) => {
                 
             if (email) {
                 if (!email.match(EmailRegex)) {
-                    console.log("Email skal indeholde @ ");
                     return res.status(400).send({ response: "Email skal indeholde @"});
-                
                 } 
             }
 
             if (phoneNumber){
                 if(!phoneNumber.match(PhoneRegex)){
-                    console.log("Telefon nr skal indeholde 8 digits ");
                     return res.status(400).send({ response: "Phonenumber should contain 8 digits"});
                 }
 
                 if (phoneNumber.length > 8) {
-                    return res.status(400).send({ response: "Zipcode must be 4 digits long"});
+                    return res.status(400).send({ response: "Phonenumber must be 8 digits long"});
                 }
             }
 
             if (zipCode) {
                 if (!zipCode.match(ZipRegex)){
-                    console.log("Zipcode skal indeholde 4 digits ");
                     return res.status(400).send({ response: "Zipcode should contain 4 digits"});
                 }
 
@@ -86,38 +81,46 @@ router.post("/createUser",(req, res) => {
                     return res.status(400).send({ response: "Zipcode must be 4 digits long"});
                 } 
             }
-        User.query().insert({
 
-            first_name: req.body.firstName, 
-            last_name: req.body.lastName,
-            email: req.body.email,
-            phone_number: req.body.phoneNumber,
-            address: req.body.address,
-            zip_code: req.body.zipCode,
-            city: req.body.city,
-            username: req.body.username,
-            password: req.body.password
-        }).then(createdUser => { 
-            return res.send( { response: `The user ${createdUser.username} was created`});
-        })
-
-
-
-                /*
-                if (password.match(/[A-Z]+/g)) {
-                    const passwordTester = password;
-                    /[A-Z]+/g.test(passwordTester)
-                    console.log(passwordTester);
-                    
-                } else {
-                    return res.status(400).send({ response: "Password should contain one capital letter"});
+            if(firstName && lastName && address && city && username) {
+                if(!firstName.match(wordRegex)) {
+                    return res.status(400).send({ response: "Cannot be null"});
                 }
-                */
-            } 
-            } 
-            
-            
-        
+
+                if(!lastName.match(wordRegex)) {
+                    return res.status(400).send({ response: "Cannot be null"});
+                }
+
+                if(!address.match(wordRegex)) {
+                    return res.status(400).send({ response: "Cannot be null"});
+                }
+
+                if(!city.match(wordRegex)) {
+                    return res.status(400).send({ response: "Cannot be null"});
+                }
+
+                if(!username.match(wordRegex)) {
+                    return res.status(400).send({ response: "Cannot be null"});
+                }
+            }
+
+            User.query().insert({
+
+                first_name: req.body.firstName, 
+                last_name: req.body.lastName,
+                email: req.body.email,
+                phone_number: req.body.phoneNumber,
+                address: req.body.address,
+                zip_code: req.body.zipCode,
+                city: req.body.city,
+                username: req.body.username,
+                password: req.body.password
+            }).then(createdUser => { 
+                return res.send( { response: `The user ${createdUser.username} was created`});
+            })
+  
+        } 
+        } 
         } catch {
                 
     }
